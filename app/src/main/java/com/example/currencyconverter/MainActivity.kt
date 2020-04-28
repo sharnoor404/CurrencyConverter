db7b740e5da4320f82940257ad31bd326c83c792
sharnoor404 <sharnoor404@gmail.com>
2020-04-28 15:18:19 +0530

    working with url

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.currencyconverter
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.net.HttpCookie
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,9 +21,22 @@ class MainActivity : AppCompatActivity() {
 //async task performs a task on a seperate thread such that the working of this task doesnt disturb the overall working of the app
     inner class Download:AsyncTask<String,Void,String>(){
 //downloading data would be done in the below function
+        //vararg stands for array of strings
         override fun doInBackground(vararg params: String?): String {
+//the output of this function is a string ,therefore we will store all the downloaded in the string 'result'
+
+
+            var result=""
+            var url:URL
+            val httpURLConnection:HttpURLConnection
+//grabbing url and setting up connection for application
+
+
+//the code for this may cause error incase url not found or if website changes its url
+    //therefore it is better to embed the code in try and catch block
             TODO("Not yet implemented")
         }
+        
 //execution of downloaded data in done in the below function
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
